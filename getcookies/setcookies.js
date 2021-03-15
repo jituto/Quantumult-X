6394241487d4a36c8880535deda4dc3b435f7872
jituto <61678622+jituto@users.noreply.github.com>
2021-03-15 09:57:54 +0800

    Add files via upload

---
+++ getcookies/setcookies.js
@@ -0,0 +1,56 @@
+const cookieName = 'qq_cookies'
+const cookieKey = 'jituto_cookie_qqdaka'
+const jituto = init()
+#const cookieVal = $request.headers['Cookie']
+if (cookieVal) {
+  if (jituto.setdata(cookieVal, cookieKey)) {
+    jituto.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    jituto.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+  }
+}
+
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+jituto.done()
